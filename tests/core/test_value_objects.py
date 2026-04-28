4bf8641349d790b5087ffbc9a632825b3a4bcdb3
Ivan <ivanmendes2005@gmail.com>
2026-04-28 13:53:25 -0300

    test: add test caso ruim value_object nome

--- tests/core/test_value_objects.py
+++ tests/core/test_value_objects.py
@@ -3,8 +3,14 @@ from unittest import TestCase
 from src.core.value_objects import Nome
 
 class TestValueObjects(TestCase):
-    def test_value_objects(self):
+
+    def test_criar_nome_valido(self):
 
         nome = Nome("John Doe")
 
         self.assertEqual("John Doe", nome.valor)
+
+    def test_criar_nome_vazio(self):
+        
+        with self.assertRaises(ValueError):
+            Nome("")
